1ef49af20bc8b13cc8fc5c7cc5445155e19c0a4a
Kevin Matson <matson.kevin@yahoo.com>
2022-03-01 21:48:32 -0600

    testing git action with test failure

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -8,4 +8,5 @@ test('renders learn react link', () => {
   const effectText = screen.getByText(/Effect/);
   expect(typeText).toBeInTheDocument();
   expect(effectText).toBeInTheDocument();
+  expect(false).toBe(true);
 });
